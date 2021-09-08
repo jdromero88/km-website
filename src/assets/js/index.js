7c3d0dcc83b5ee05c28e3f6d1f0d9fbd2f68b375
jdromero88 <jdromerov88@gmail.com>
2021-09-07 23:55:31 -0400

    Webpack first config is connected and working

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,10 +1,24 @@
-console.log('hola')
+import _ from 'lodash';
 
+console.log('hola')
 const TODAY = new Date()
 const YEAR = TODAY.getFullYear()
-
 console.log(YEAR)
 
-const yearEl = document.getElementById('year')
-console.log(yearEl)
-yearEl.innerText = YEAR
+function yearElement() {
+  const yearEl = document.getElementById('year')
+  yearEl.innerText = YEAR
+  return yearEl
+}
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(yearElement());
+document.body.appendChild(component());
